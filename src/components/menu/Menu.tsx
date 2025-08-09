03e990ace279d3218b45b5a6c4ab23111b55b365
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-08-09 17:37:22 -0300

    começando

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,12 @@
+export function Menu() {
+    return (
+        <div className="border-2 border-red-500">
+            <section className="flex justify-between mx-10 mt-10">
+                <div className="text-4xl">FS</div>
+                <div>
+                    <img src="/menu.png" alt="" />
+                </div>
+            </section>
+        </div>
+    );
+}
---
+++ src/data/photoList.ts
@@ -0,0 +1,21 @@
+import { Photo } from "@/types/Photo";
+
+export const photoList: Photo[] = [
+    {id: 1, url: 'nike.png'},
+    {id: 2, url: 'SavoryGarden.png'},
+    {id: 3, url: 'gym.png'},
+    {id: 4, url: 'apple-watch.png'},
+    {id: 5, url: 'medi-center.png'},
+    {id: 6, url: 'Starbucks.png'},
+    {id: 7, url: 'pizzas.png'},
+    {id: 8, url: 'jogos.png'},
+    {id: 9, url: 'urna.png'},
+    {id: 10, url: 'relogio.png'},
+    {id: 11, url: 'clima.png'},
+    {id: 12, url: 'lakers.png'},
+    {id: 13, url: 'WWE.png'},
+    {id: 14, url: 'spotify.jpg'},
+    {id: 15, url: 'tribute.png'},
+    {id: 16, url: 'formulario.png'},
+    {id: 17, url: 'notas.jpeg'},
+];
