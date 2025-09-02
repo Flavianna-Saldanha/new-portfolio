8881eb25d7111d256b25b4cf606a9b122b48b641
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-09-02 13:32:13 -0300

    mudanças

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,32 @@
+import Image from "next/image";
+
 export const Header = () => {
     return (
-        <div className="h-[calc(100vh-90px)] flex flex-col justify-between after:content-[''] after:absolute after:top-0 after:right-0 after:w-[42%] after:h-screen after:bg-white after:[clip-path:polygon(100%_33%,100%_0%,100%_100%,0%_100%)] after:-z-10">
-            <div>
-                <div>
-                    <div className="absolute left-60 top-50 w-16 h-7 bg-gray-200 rounded-xl flex justify-center items-center">
-                        <img className="w-5" src="assets/woman-technologist-light-skin-tone.png" alt="" />
+        <div className="after:content-[''] after:absolute after:top-0 after:right-0 after:w-[42%] after:h-screen after:bg-white after:[clip-path:polygon(100%_33%,100%_0%,100%_100%,0%_100%)] after:-z-10">
+            <div className="h-[calc(100vh-90px)] border border-red-600 flex flex-col justify-between items-center">
+                <div className="mt-28">
+                    <div className="bg-white w-15 h-6 rounded-xl flex justify-center items-center mb-4 ml-40">
+                        <Image
+                            src="/assets/woman-technologist-light-skin-tone.png"
+                            alt="woman technologist"
+                            width={20}
+                            height={20}
+                        />
+                        <div className="absolute left-20 mt-8 ml-40 w-0 h-0 border-l-8 border-l-transparent border-t-8 border-t-white"/>
                     </div>
-                    <div className="absolute left-[270px] top-56 w-0 h-0 border-l-[10px] border-l-transparent border-t-[10px] border-t-gray-200"/>
+                    <div>
+                        <div className="w-72 m-auto font-bold text-3xl text-center uppercase drop-shadow-[8px_8px_5px_rgb(0,0,0)]">I'm Flavianna Saldanha</div>
+                        <div className="italic font-opensans uppercase text-[10px] text-end text-gray-500 drop-shadow-[8px_8px_5px_black]">Front-end Developer</div>
+                    </div>
                 </div>
-                
-                <div className="w-3/4 m-auto mt-36 text-3xl text-center uppercase font-bold drop-shadow-[8px_8px_5px_rgb(0,0,0)] overflow-hidden leading-tight">
-                    I'm Flavianna Saldanha
+                <div className="drop-shadow-[8px_8px_5px_black]">
+                    <Image
+                        src="/assets/foto-ap.png"
+                        alt="woman technologist"
+                        width={300}
+                        height={300}
+                    />
                 </div>
-                <div className="text-end w-3/4 mr-42 text-gray-500 uppercase text-[10px] italic font-opensans drop-shadow-[8px_8px_5px_black]">Front-end Developer</div>
-            </div>
-
-            <div className="drop-shadow-[8px_8px_5px_black]">
-                <img className="w-3/4 m-auto" src="/assets/foto-ap.png" alt="" />
             </div>
         </div>
     );
