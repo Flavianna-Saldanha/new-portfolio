086e66170708f08aa4b207ec7c78ae6fbca9949c
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-09-18 09:17:32 -0300

    ajustes finais

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,12 +8,12 @@ export const About = () => {
                     <img 
                         src="/assets/IMG_1537 (3).png" 
                         alt="Minha foto" 
-                        className="w-64 lg:w-[425px] lg:ml-5"
+                        className="w-64 lg:w-[350px]"
                     />
                 </div>
-                <div className="mx-10 text-justify flex flex-col gap-12 lg:w-2xl lg:gap-9">
-                    <div className="uppercase hidden text-center font-bold text-2xl lg:block lg:text-start lg:text-3xl">Sobre mim</div>
-                    <div className="leading-8 text-[18px] lg:text-[18px] lg:leading-8">Sou desenvolvedora front-end, apaixonada por criar interfaces web intuitivas e experiências digitais marcantes. Estou sempre em busca de aprendizado contínuo, dominando as bases de HTML, CSS e JavaScript. Por meio de projetos pessoais, tenho aprimorado minhas habilidades em responsividade, design de layout e interatividade. Possuo uma forte capacidade de aprendizado e sou proativa na busca por novas soluções.</div>
+                <div className="mx-10 text-justify flex flex-col gap-12 lg:w-lg lg:gap-8">
+                    <div className="uppercase hidden text-center font-bold text-2xl lg:block lg:text-start lg:text-2xl">Sobre mim</div>
+                    <div className="leading-8 text-[18px]lg:text-[15px] lg:leading-8">Sou desenvolvedora front-end, apaixonada por criar interfaces web intuitivas e experiências digitais marcantes. Estou sempre em busca de aprendizado contínuo, dominando as bases de HTML, CSS e JavaScript. Por meio de projetos pessoais, tenho aprimorado minhas habilidades em responsividade, design de layout e interatividade. Possuo uma forte capacidade de aprendizado e sou proativa na busca por novas soluções.</div>
                     <div className="text-center lg:text-start">
                         <a
                             href="https://drive.google.com/file/d/1TLkD-NGPNaGa766hdk3nY0-9dEabVSje/view?usp=sharing"
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,24 +4,24 @@ export const Header = () => {
     return (
         <div className="after:content-[''] after:absolute after:top-0 after:right-0 after:w-[42%] after:h-screen after:bg-white after:[clip-path:polygon(100%_33%,100%_0%,100%_100%,0%_100%)] after:-z-10 lg:after:[clip-path:polygon(75%_0%,100%_0%,100%_100%,0%_100%)]">
             <div className="overflow-y-hidden relative h-[calc(100vh-90px)] flex flex-col justify-between items-center lg:flex-row lg:justify-around lg:items-start">
-                <div className="mt-50 relative lg:mt-60 lg:right-72 lg:w-96">
-                    <div className="relative bg-white w-15 h-6 rounded-2xl flex justify-center items-center mb-4 ml-40 lg:w-16 lg:h-8 lg:ml-[200px]">
+                <div className="mt-40 relative lg:mt-60 lg:right-72 lg:w-96">
+                    <div className="relative bg-white w-12 h-5 rounded-2xl flex justify-center items-center mb-4 ml-40 lg:w-16 lg:h-8 lg:ml-[200px]">
                         <Image
                             src="/assets/woman-technologist-light-skin-tone.png"
                             alt="woman technologist"
                             width={120}
                             height={120}
-                            className="w-5 h-5 lg:w-6 lg:h-6"
+                            className="w-3 h-3 lg:w-6 lg:h-6"
                         />
                     </div>
 
-                    <div className="absolute top-6 left-[190px] -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-t-8 border-t-white lg:border-l-[10px] lg:border-t-[10px] lg:left-[230px] lg:top-8" />
+                    <div className="absolute top-5 left-[190px] -translate-x-1/2 w-0 h-0 border-l-8 border-l-transparent border-t-8 border-t-white lg:border-l-[10px] lg:border-t-[10px] lg:left-[230px] lg:top-8" />
                     
                     <div>
-                        <div className="w-72 overflow-y-hidden m-auto font-bold text-2xl text-center uppercase drop-shadow-[8px_8px_5px_rgb(0,0,0)] sm:text-4xl lg:w-auto lg:text-4xl xl:text-5xl">
+                        <div className="w-72 overflow-y-hidden m-auto font-bold text-[22px] text-center uppercase drop-shadow-[8px_8px_5px_rgb(0,0,0)] sm:text-3xl lg:w-auto md:text-2xl lg:text-4xl xl:text-5xl">
                             I'm Flavianna Saldanha
                         </div>
-                        <div className="italic font-opensans uppercase text-[10px] mr-5 text-end text-gray-500 drop-shadow-[8px_8px_5px_black]">
+                        <div className="italic font-opensans uppercase text-[8px] mr-5 text-end text-gray-500 drop-shadow-[8px_8px_5px_black]">
                             Front-end Developer
                         </div>
                     </div>
@@ -33,7 +33,7 @@ export const Header = () => {
                         alt="woman technologist"
                         width={1200}
                         height={1200}
-                        className="m-auto w-3/4 sm:w-7/12 lg:max-h-screen lg:w-[340px] xl:w-[500px] lg:object-contain"
+                        className="m-auto w-[47%] md:w-[28%] sm:w-[38%] lg:max-h-screen lg:w-[340px] xl:w-[500px] lg:object-contain"
                     />
                 </div>
             </div>
