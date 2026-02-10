4fb084826be5c1e902dd19fdc40fefe2b9c45e94
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-02-10 15:52:20 -0300

    add `skills`

--- src/data/skillsList.ts
+++ src/data/skillsList.ts
@@ -6,6 +6,7 @@ export const skillsList: Skills[] = [
         { src: "/assets/js (1).png", alt: "JavaScript" },
         { src: "/assets/typescript.png", alt: "TypeScript" },
         { src: "/assets/tailwindcss.png", alt: "TailwindCSS" },
+				{ src: "/assets/MUI.png", alt: "MUI" },
         { src: "/assets/react.png", alt: "React" },
         { src: "/assets/nextjs-icon-svgrepo-com.png", alt: "Next.js" },
         { src: "/assets/mysql.png", alt: "MySQL" },
